50e20db28279ad01676d17f685a09d517c192404
tohsaka888 <156132264@qq.com>
2024-03-04 22:25:56 +0800

    ✨ feat(api-doc): support api doc group

--- services/openapi/contract.ts
+++ services/openapi/contract.ts
@@ -1,17 +1,43 @@
 // contract.ts
 import { initContract } from "@ts-rest/core";
 import { generateOpenApi } from "@ts-rest/open-api";
+import { z } from "zod";
+import { extendZodWithOpenApi } from "@anatine/zod-openapi";
+
+extendZodWithOpenApi(z);
 
 const c = initContract();
 
-export const contract = c.router({
+const userContract = c.router({
   login: {
     method: "POST",
     path: "/api/users/login",
     responses: {
-      200: c.type<Omit<UserType.User, "id">>(),
+      200: z.object({
+        token: z.string().openapi({
+          title: "JWT token",
+          description: "JWT token for authentication",
+          example: "",
+        }),
+        success: z.boolean().openapi({
+          title: "success flag",
+          description: "sucess if flag is true, else the result is fail",
+          example: true,
+        }),
+      }),
     },
-    body: c.type<Omit<UserType.User, "password">>(),
+    body: z.object({
+      email: z.string().openapi({
+        title: "email",
+        description: "user's email",
+        example: "156132264@qq.com",
+      }),
+      password: z.string().openapi({
+        title: "password",
+        description: "user's password",
+        example: "swy156132264",
+      }),
+    }),
     summary: "Login to HuiZhen",
   },
   register: {
@@ -25,10 +51,27 @@ export const contract = c.router({
   },
 });
 
-export const OpenAPIV1 = generateOpenApi(contract, {
-  info: {
-    title: "Api Document for HuiZhen",
-    version: "1.0.0",
-    description: "Android App HuiZhen's Api Document",
-  },
+const contract = c.router({
+  Users: userContract,
 });
+
+export const OpenAPIV1 = generateOpenApi(
+  contract,
+  {
+    info: {
+      title: "Api Document for HuiZhen",
+      version: "1.0.0",
+      // description: "Android App HuiZhen's Api Document",
+    },
+    tags: [
+      {
+        name: "Users",
+        description: "Users Interface, such as: login/register",
+      },
+    ],
+  },
+  {
+    setOperationId: true,
+    jsonQuery: true,
+  }
+);
